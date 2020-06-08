cc54ef9101b5008499e3f7c83f12ca02cc497322
CutShooter <ronikrocks2000@gmail.com>
2020-06-08 19:41:33 +0530

    Full Project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Expenses App
+
+A simple flutter application on daily wage of expenses.
+
+## Getting Started
+![](assets/ReadmeImages/firstTimeOpeningScreenshot.png)
+This is the when you open the app(also after closing it and reopening it)
+
+Let's go through the app. The app has :
+- Chart showing the graphical preview of the expenses last week.
+- The picture where it is showing 'No transactions added yet!', will be converted to a list shown later.
+- A Floating Action Button or the button up in the app bar will open a modal bottom sheet which help us to add a transaction.
+
+ ## Pressing '+' icon
+ ![](assets/ReadmeImages/Bottom%20Sheet.png)
+It opens an bottom sheet which has the following components :
+
+- Title will contain the name of item
+- Amount the price in dollars
+- Choose Date is the flat button and pressing on it will pop up a date picker. On picking date, the choosed date will be seen instead of 'No date choosen'.
+- Add transaction will add the item to the list.
+
+## Final View
+![](assets/ReadmeImages/FinalView.png)
+The final view is as follows:
+- The chart shows the relative expenditure done in the last week with respect to day.
+- The list item shows the price in the circle, title and date. On pressing the delete icon that item will be deleted.
+- The list item is scrollable, so we add as many items as we want!
+
+## Future Improvements
+- The items are not stored permanently. On closing and reopening the app, All the data is lost.
+- More interactive bottom sheet. 
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: transactions.isEmpty
+          ? LayoutBuilder(
+              builder: (context, contraints) {
+                return Column(
+                  children: <Widget>[
+                    Text(
+                      'No transactions added yet!',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                        height: contraints.maxHeight * 0.6,
+                        child: Image.asset(
+                          'assets/images/zzz.png',
+                          fit: BoxFit.cover,
+                        )),
+                  ],
+                );
+              },
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: MediaQuery.of(context).size.width >= 430
+                        ? FlatButton.icon(
+                            textColor: Theme.of(context).errorColor,
+                            icon: Icon(Icons.delete),
+                            onPressed: () => deleteTx(transactions[index].id),
+                            label: Text('delete'),
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.delete),
+                            color: Theme.of(context).errorColor,
+                            onPressed: () => deleteTx(transactions[index].id),
+                          ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
+  }
+}
